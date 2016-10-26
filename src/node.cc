80e9fc96296433deaf9c4838362bcd89b430f045
Michael Cornacchia <cornacch@ca.ibm.com>
2016-10-26 15:33:34 -0400

    Copy VSNPrintFASCII from v8 to node.
    Test usage for an error message to also fix a test.

--- src/node.cc
+++ src/node.cc
@@ -216,6 +216,100 @@ static void IdleImmediateDummy(uv_idle_t* handle) {
   // TODO(bnoordhuis) Maybe make libuv accept NULL idle callbacks.
 }
 
+
+#pragma convert("IBM-1047")
+inline const uint8_t& Ascii2Ebcdic(const char letter) {
+  static unsigned char a2e[256] = {
+  0,1,2,3,55,45,46,47,22,5,21,11,12,13,14,15,
+  16,17,18,19,60,61,50,38,24,25,63,39,28,29,30,31,
+  64,79,127,123,91,108,80,125,77,93,92,78,107,96,75,97,
+  240,241,242,243,244,245,246,247,248,249,122,94,76,126,110,111,
+  124,193,194,195,196,197,198,199,200,201,209,210,211,212,213,214,
+  215,216,217,226,227,228,229,230,231,232,233,74,224,90,95,109,
+  121,129,130,131,132,133,134,135,136,137,145,146,147,148,149,150,
+  151,152,153,162,163,164,165,166,167,168,169,192,106,208,161,7,
+  32,33,34,35,36,21,6,23,40,41,42,43,44,9,10,27,
+  48,49,26,51,52,53,54,8,56,57,58,59,4,20,62,225,
+  65,66,67,68,69,70,71,72,73,81,82,83,84,85,86,87,
+  88,89,98,99,100,101,102,103,104,105,112,113,114,115,116,117,
+  118,119,120,128,138,139,140,141,142,143,144,154,155,156,157,158,
+  159,160,170,171,172,173,174,175,176,177,178,179,180,181,182,183,
+  184,185,186,187,188,189,190,191,202,203,204,205,206,207,218,219,
+  220,221,222,223,234,235,236,237,238,239,250,251,252,253,254,255
+  };
+  return a2e[letter];
+}
+
+
+inline int GetFirstFlagFrom(const char* format_e, int start = 0) {
+  int flag_pos = start;
+  for (; format_e[flag_pos] != '\0' && format_e[flag_pos] != '%'; flag_pos++); // find the first flag
+  return flag_pos;
+}
+
+
+int VSNPrintFASCII(char* out, int length, const char* format_a, ...) {
+  va_list args;
+  va_start(args, format_a);
+
+  int bytes_written = 0, bytes_remain = length;
+  size_t format_len = strlen(format_a);
+  char buffer_e[format_len + 1];
+  char * format_e = buffer_e;
+  memcpy(format_e, format_a, format_len + 1);
+  __a2e_s(format_e);
+  int first_flag = GetFirstFlagFrom(format_e);
+  if (first_flag > 0) {
+    int size = snprintf(out, length, "%.*s", first_flag, format_e);
+    CHECK(size >= 0);
+    bytes_written += size;
+    bytes_remain = length - bytes_written;
+  }
+  format_e += first_flag;
+  if (format_e[0] == '\0') return bytes_written;
+
+  do {
+    int next_flag = GetFirstFlagFrom(format_e, 2);
+    char tmp = format_e[next_flag];
+    int ret = 0;
+    format_e[next_flag] = '\0';
+    char flag = format_e[1];
+    if (flag == 's') {
+      // convert arg
+      char * str = va_arg(args, char *);
+      size_t str_len = strlen(str);
+      char str_e[str_len + 1];
+      memcpy(str_e, str, str_len + 1);
+      __a2e_s(str_e);
+      ret = snprintf(out + bytes_written, bytes_remain, format_e, str_e);
+    } else if (flag == 'c') {
+      ret = snprintf(out + bytes_written, bytes_remain, format_e, Ascii2Ebcdic(va_arg(args, char)));
+    } else {
+      ret = snprintf(out + bytes_written, bytes_remain, format_e, args);
+    }
+    CHECK(ret >= 0);
+    bytes_written += ret;
+    bytes_remain = length - bytes_written;
+    format_e[next_flag] = tmp;
+    format_e += next_flag;
+    bytes_remain = length - bytes_written;
+  } while (format_e[0] != '\0' || bytes_remain <= 0);
+
+  __e2a_s(out);
+  return bytes_written;
+}
+
+
+int SNPrintFASCII(char * out, int length, const char* format_a, ...) {
+  va_list args;
+  va_start(args, format_a);
+  int ret = VSNPrintFASCII(out, length, format_a, args);
+  va_end(args);
+  return ret;
+}
+#pragma convert(pop)
+
+
 #pragma convert("ISO8859-1")
 static inline const char *errno_string(int errorno) {
 #define ERRNO_CASE(e)  case e: return #e;
@@ -2250,9 +2344,9 @@ static void Binding(const FunctionCallbackInfo<Value>& args) {
     cache->Set(module, exports);
   } else {
     char errmsg[1024];
-    snprintf(errmsg,
+    VSNPrintFASCII(errmsg,
              sizeof(errmsg),
-             "\x4e\x6f\x20\x73\x75\x63\x68\x20\x6d\x6f\x64\x75\x6c\x65\x3a\x20\x6c\xa2",
+             "No such module: %s",
              *module_v);
     return env->ThrowError(errmsg);
   }
