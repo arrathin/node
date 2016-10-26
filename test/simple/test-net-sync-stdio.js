312c8b1ee4f93263cfee5279db2f98bcb7018138
Junliang Yan <jyan@ca.ibm.com>
2016-10-26 17:38:02 -0400

    fix again test-net-sync-stdio.js

--- test/simple/test-net-sync-stdio.js
+++ test/simple/test-net-sync-stdio.js
@@ -30,16 +30,17 @@ if (process.argv[2] === 'child') {
   process.stdout.write(big, function () {
     process.exit(42);
   });
+} else {
+
+  var stdio = ['inherit', 'pipe', 'inherit'];
+  var proc = spawn(process.execPath, [__filename, 'child'], { stdio: stdio });
+
+  var chunks = [];
+  proc.stdout.setEncoding('utf8');
+  proc.stdout.on('data', chunks.push.bind(chunks));
+
+  proc.on('exit', common.mustCall(function(exitCode) {
+    assert.equal(exitCode, 42);
+    assert.equal(chunks.join(''), big);
+  }));
 }
-
-var stdio = ['inherit', 'pipe', 'inherit'];
-var proc = spawn(process.execPath, [__filename, 'child'], { stdio: stdio });
-
-var chunks = [];
-proc.stdout.setEncoding('utf8');
-proc.stdout.on('data', chunks.push.bind(chunks));
-
-proc.on('exit', common.mustCall(function(exitCode) {
-  assert.equal(exitCode, 42);
-  assert.equal(chunks.join(''), big);
-}));
