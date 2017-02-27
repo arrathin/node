ed28ad1358aff4b4453cf90a3062989182853eff
jBarz <jbarboza@ca.ibm.com>
2017-02-27 08:39:34 -0500

    zos, test: increase stack size

--- test/simple/test-process-exec-argv.js
+++ test/simple/test-process-exec-argv.js
@@ -25,7 +25,10 @@ var spawn = require('child_process').spawn;
 if (process.argv[2] === 'child') {
   process.stdout.write(JSON.stringify(process.execArgv));
 } else {
-  var execArgv = ['--harmony_proxies', '--stack-size=128'];
+  if (process.platform === 'os390')
+    var execArgv = ['--harmony_proxies', '--stack-size=1024'];
+  else
+    var execArgv = ['--harmony_proxies', '--stack-size=64'];
   var args = [__filename, 'child', 'arg0'];
   var child = spawn(process.execPath, execArgv.concat(args));
   var out = '';
