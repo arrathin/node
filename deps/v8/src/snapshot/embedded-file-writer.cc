0c3487400e61ee1af80920ba9a59bbb1e0f75d4f
ccw <ccw.280231@ca.ibm.com>
2019-07-26 16:02:56 -0400

    zos generate embedded

--- deps/v8/src/snapshot/embedded-file-writer.cc
+++ deps/v8/src/snapshot/embedded-file-writer.cc
@@ -98,6 +98,8 @@ const char* DirectiveAsString(DataDirective directive) {
     default:
       UNREACHABLE();
   }
+#elif defined(V8_OS_ZOS)
+  UNREACHABLE(); // should never be called
 #else
   switch (directive) {
     case kByte:
@@ -319,6 +321,154 @@ int PlatformDependentEmbeddedFileWriter::IndentedDataDirective(
   return fprintf(fp_, "  %s ", DirectiveAsString(directive));
 }
 
+#elif defined(V8_OS_ZOS)
+
+static int hlasm_fprintf_line(FILE* fp, const char* fmt, ...) {
+  int ret;
+  char buffer[4096];
+  int offset = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  ret = vsnprintf(buffer, 4096, fmt, ap);
+  va_end(ap);
+  if (ret > 71) {
+    offset += fwrite(buffer + offset, 1, 71, fp);
+    fwrite("-\n", 1, 2, fp);  // write continuation mark
+    ret -= 71;
+    while (ret > 56) {
+      fwrite("               ", 1, 15, fp);  // indent 15
+      offset += fwrite(buffer + offset, 1, 56, fp);
+      fwrite("-\n", 1, 2, fp);  // write continuation mark
+      ret -= 56;
+    }
+    if (ret > 0) {
+      fwrite("               ", 1, 15, fp);  // indent 15
+      offset += fwrite(buffer + offset, 1, ret, fp);
+    }
+  } else {
+    offset += fwrite(buffer + offset, 1, ret, fp);
+  }
+  return ret;
+}
+
+void PlatformDependentEmbeddedFileWriter::SectionText() {
+  // CSECT
+}
+
+void PlatformDependentEmbeddedFileWriter::SectionData() {
+  // DATA
+}
+
+void PlatformDependentEmbeddedFileWriter::SectionRoData() {
+  // CSECT RO
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareUint32(const char* name,
+                                                        uint32_t value) {
+  fprintf(fp_,
+          "\
+&suffix SETA &suffix+1\n\
+CEECWSA LOCTR\n\
+AL&suffix ALIAS C'%s'\n\
+C_WSA64 CATTR DEFLOAD,RMODE(64),PART(AL&suffix)\n\
+AL&suffix XATTR REF(DATA),LINKAGE(XPLINK),SCOPE(EXPORT)\n\
+ dc F'%d'\n\
+C_WSA64 CATTR PART(PART1)\n\
+LBL&suffix DC AD(AL&suffix)\n\
+",
+          name,
+          value);
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclarePointerToSymbol(
+    const char* name, const char* target) {
+  fprintf(fp_,
+          "\
+&suffix SETA &suffix+1\n\
+CEECWSA LOCTR\n\
+AL&suffix ALIAS C'%s'\n\
+C_WSA64 CATTR DEFLOAD,RMODE(64),PART(AL&suffix)\n\
+AL&suffix XATTR REF(DATA),LINKAGE(XPLINK),SCOPE(EXPORT)\n\
+ dc AD(%s)\n\
+C_WSA64 CATTR PART(PART1)\n\
+LBL&suffix DC AD(AL&suffix)\n\
+",
+          name,
+          target);
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareSymbolGlobal(
+    const char* name) {
+  hlasm_fprintf_line(fp_, "* Global Symbol %s\n", name);
+}
+
+void PlatformDependentEmbeddedFileWriter::AlignToCodeAlignment() {
+  // CODE alignment
+}
+
+void PlatformDependentEmbeddedFileWriter::AlignToDataAlignment() {
+  // DATA alignment
+}
+
+void PlatformDependentEmbeddedFileWriter::Comment(const char* string) {
+  hlasm_fprintf_line(fp_, "* %s\n", string);
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareLabel(const char* name) {
+  hlasm_fprintf_line(fp_, "*--------------------------------------------\n");
+  hlasm_fprintf_line(fp_, "* Label %s\n", name);
+  hlasm_fprintf_line(fp_, "*--------------------------------------------\n");
+  hlasm_fprintf_line(fp_, "%s ds 0h\n", name);
+}
+
+void PlatformDependentEmbeddedFileWriter::SourceInfo(int fileid,
+                                                     const char* filename,
+                                                     int line) {
+  hlasm_fprintf_line(fp_, "* line %d \"%s\"\n", line, filename);
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareFunctionBegin(
+    const char* name) {
+  hlasm_fprintf_line(fp_, "*--------------------------------------------\n");
+  hlasm_fprintf_line(fp_, "* Builtin %s\n", name);
+  hlasm_fprintf_line(fp_, "*--------------------------------------------\n");
+  int len = strlen(name);
+  if (len > 63) {  // max name length 63
+    char* newname = (char*)alloca(64);
+    strcpy(newname, "_bi_");
+    strcpy(newname + 4, name + (len - 59));
+    hlasm_fprintf_line(fp_, "%s ds 0h\n", newname);
+  } else
+    hlasm_fprintf_line(fp_, "%s ds 0h\n", name);
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareFunctionEnd(const char* name) {
+}
+
+int PlatformDependentEmbeddedFileWriter::HexLiteral(uint64_t value) {
+  return fprintf(fp_, "%.16lx", value);
+}
+
+void PlatformDependentEmbeddedFileWriter::FilePrologue() {
+  fprintf(fp_, "\
+&C SETC 'embed'\n\
+ SYSSTATE AMODE64=YES\n\
+&C csect\n\
+&C amode 64\n\
+&C rmode 64\n");
+}
+
+void PlatformDependentEmbeddedFileWriter::DeclareExternalFilename(
+    int fileid, const char* filename) {}
+
+void PlatformDependentEmbeddedFileWriter::FileEpilogue() {
+  fprintf(fp_, " end\n");
+}
+
+int PlatformDependentEmbeddedFileWriter::IndentedDataDirective(
+    DataDirective directive) {
+  return hlasm_fprintf_line(fp_, "  %s ", DirectiveAsString(directive));
+}
 // V8_OS_WIN (MSVC)
 // -----------------------------------------------------------------------------
 
