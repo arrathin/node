1f5f7aef91391f8a5a651cb59fd220ac0c2a94d9
Igor Todorovski <itodorov@ca.ibm.com>
2019-09-09 14:29:48 -0400

    Increase stack size slightly for zos for test-async-wrap-pop-id-during-load.js

--- test/parallel/test-async-wrap-pop-id-during-load.js
+++ test/parallel/test-async-wrap-pop-id-during-load.js
@@ -14,11 +14,19 @@ if (process.argv[2] === 'async') {
 const assert = require('assert');
 const { spawnSync } = require('child_process');
 
-const ret = spawnSync(
-  process.execPath,
-  ['--stack_size=150', __filename, 'async'],
-  { maxBuffer: Infinity }
-);
+var ret;
+if (process.platform === 'zos')
+    ret = spawnSync(
+      process.execPath,
+      ['--stack_size=350', __filename, 'async'],
+      { maxBuffer: Infinity }
+    );
+else
+    ret = spawnSync(
+      process.execPath,
+      ['--stack_size=150', __filename, 'async'],
+      { maxBuffer: Infinity }
+    );
 assert.strictEqual(ret.status, 0,
                    `EXIT CODE: ${ret.status}, STDERR:\n${ret.stderr}`);
 const stderr = ret.stderr.toString('utf8', 0, 2048);
