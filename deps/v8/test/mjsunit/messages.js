5c5dcfedc6c7106801cb3aacff9ddd91658e78a8
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2019-09-16 16:58:03 -0400

    v8: merge v8z mjsunit tests stack size increase
    This commit aggregates test changes made in v8z branch 6.2-lkgr.zos.
    For all of these tests, the stack size was set to a value that is too
    small for z/OS, so they need to be increased accordingly.

--- deps/v8/test/mjsunit/messages.js
+++ deps/v8/test/mjsunit/messages.js
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --allow-natives-syntax --stack-size=100 --harmony
+// On z/OS, this test needs a stack size of at least 204 kBytes.
+// Flags: --allow-natives-syntax --stack-size=220 --harmony
 
 function test(f, expected, type) {
   try {
