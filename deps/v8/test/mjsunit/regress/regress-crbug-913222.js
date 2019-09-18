ecefa442f277cc945e20e8cde53a89642d433697
Shuowang (Wayne) Zhang <shuowang.zhang@ibm.com>
2019-09-18 11:24:13 -0400

    v8: increase stack size of various mjsunit tests
    These tests need a stack size of at least 204 kBytes to run on z/OS,
    otherwise they will immediately exceed the maximum call stack size.

--- deps/v8/test/mjsunit/regress/regress-crbug-913222.js
+++ deps/v8/test/mjsunit/regress/regress-crbug-913222.js
@@ -2,7 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Flags: --stack-size=100
+// On z/OS, this test needs a stack size of at least 204 kBytes.
+// Flags: --stack-size=220
 __v_0 = '(function() {\n';
 for (var __v_1 = 0; __v_1 < 10000; __v_1++) {
     __v_0 += '  return function() {\n';
