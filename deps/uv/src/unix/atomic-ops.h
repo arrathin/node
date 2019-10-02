3e5207c42f700c0890258ef80651613e9c30022f
ccw <ccw.280231@ca.ibm.com>
2019-10-02 13:29:01 -0400

    use CS instead of PLO

--- deps/uv/src/unix/atomic-ops.h
+++ deps/uv/src/unix/atomic-ops.h
@@ -42,12 +42,8 @@ UV_UNUSED(static int cmpxchgi(int* ptr, int oldval, int newval)) {
   __compare_and_swap(ptr, &oldval, newval);
   return out;
 #elif defined(__MVS__)
-  unsigned int op4;
-  if (__plo_CSST(ptr, (unsigned int*) &oldval, newval,
-                (unsigned int*) ptr, *ptr, &op4))
-    return oldval;
-  else
-    return op4;
+   __asm(" cs %0,%2,%1 \n " : "+r"(oldval), "+m"(*ptr) : "r"(newval) :);
+   return oldval;
 #elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
   return atomic_cas_uint((uint_t *)ptr, (uint_t)oldval, (uint_t)newval);
 #else
