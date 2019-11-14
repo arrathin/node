abc0f044b00bc3aa245305f202cbfbedbc059741
Igor Todorovski <itodorov@ca.ibm.com>
2019-11-14 15:22:32 -0500

    Enable inline builds for all v8 source

--- deps/v8/src/heap/mark-compact.cc
+++ deps/v8/src/heap/mark-compact.cc
@@ -1,10 +1,6 @@
 // Copyright 2012 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#if V8_TARGET_ARCH_S390X
-#pragma options("-qNOINLINE")
-#endif
-
 #include "src/heap/mark-compact.h"
 
 #include <unordered_map>
