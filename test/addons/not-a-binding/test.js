ae4f684ad6ff57e17214304f581447c8ce3f1b74
Igor Todorovski <itodorov@ca.ibm.com>
2019-11-18 20:19:58 -0500

    Remove z/OS specific conditional for test/addons/not-a-binding as it is no longer needed

--- test/addons/not-a-binding/test.js
+++ test/addons/not-a-binding/test.js
@@ -4,8 +4,4 @@ const assert = require('assert');
 
 let re = /^Error: Module did not self-register\.$/;
 
-if (common.isZOS) {
-  re = /^Error: CEE3552S/;
-}
-
 assert.throws(() => require(`./build/${common.buildType}/binding`), re);
